65a5fe5f7ebe77ba0b324e5d31cfef82987f71b6
MADHAN0803 <madhanrugu555@gmail.com>
2022-07-10 10:33:11 +0530

    vikram

---
+++ src/employeeDetails/EmployeeInfo.java
@@ -0,0 +1,22 @@
+package employeeDetails;
+
+public class EmployeeInfo {
+	private void employeeName() {
+		System.out.println("Employee Name is MADHAN");
+	}
+	private void employeeAge() {
+		System.out.println("Age is 24");
+
+	}
+	public static void main(String[] args) {
+		EmployeeInfo d = new EmployeeInfo();
+		d.employeeName();d.employeeAge();
+	}
+	
+ }
+	
+
+	
+
+
+
